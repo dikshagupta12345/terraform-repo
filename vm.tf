15f8e270dc3770ed83c62033bde8c5858bbc1051
Your Name <you@example.com>
2025-08-06 22:23:11 +0530

    corrected

--- vm.tf
+++ vm.tf
@@ -26,32 +26,3 @@
     account_id   = "custom-sa"
     display_name = "Custom SA for VM Instance"
   }
-
-resource "google_compute_instance" "instance" {
-  name         = "${var.instance_name}-${count.index + 1}"
-  machine_type = var.machine_type
-  zone         = var.zone
-  count        = var.instance_count
-
-  boot_disk {
-    initialize_params {
-      image = var.image
-    }
-  }
-
-  network_interface {
-    network = "default"
-  }
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.custom_sa.email
-    scopes = ["cloud-platform"]
-  }
-
-}
-
-resource "google_service_account" "custom_sa" {
-  account_id   = "custom-sa"
-  display_name = "Custom SA for VM Instance"
-}
