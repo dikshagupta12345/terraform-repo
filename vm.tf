0036e324eacde452a1c24bffb50e9d35f78fba20
dikshagupta12345 <diksha.cloud2025@gmail.com>
2025-08-29 19:16:33 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,29 +1,4 @@
-  resource "google_compute_instance" "instance" {
-    name         = "${var.instance_name}-${count.index + 1}"
-    machine_type = var.machine_type
-    zone         = var.zone
-    count        = var.instance_count
-
-    boot_disk {
-      initialize_params {
-        image = var.image
-      }
-    }
-
-    network_interface {
-      network = google_compute_network.myvpc.name
-      subnetwork=google_compute_subnetwork.subnet1.name
-    }
-
-    service_account {
-      # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-      email  = google_service_account.custom_sa.email
-      scopes = ["cloud-platform"]
-    }
-
-  }
-
-  resource "google_service_account" "custom_sa" {
-    account_id   = "custom-sa"
-    display_name = "Custom SA for VM Instance"
-  }
+module "vm_module" {
+  source      = "git::https://github.com/dikshagupta12345/main-modules.git?ref=main"
+  bucket_name = "my-test-bucket-123"
+}
