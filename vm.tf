a7bb05dfcb48be67465b53492fcbeda5d8cc8ee5
dikshagupta12345 <diksha.cloud2025@gmail.com>
2025-08-29 19:39:16 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,4 +1,11 @@
 module "vm_module" {
   source      = "git::https://github.com/dikshagupta12345/main-modules.git?ref=main"
-  bucket_name = "my-test-bucket-123"
+  instance_name  = "app"
+  machine_type   = "e2-small"
+  region         = "us-central1"
+  zone           = "us-central1-a"
+  project_id     = "innate-bucksaw-464410-e8"
+  instance_count = 1
+  network = "myvpc"
+  subnetwork = "subnetwork1"
 }
