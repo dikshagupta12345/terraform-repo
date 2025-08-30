c362bb63436247d083b7e905f840bffa82d2f33f
dikshagupta12345 <diksha.cloud2025@gmail.com>
2025-08-30 22:32:20 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,8 +1,15 @@
-# ----------------
-# VM Instances
-# ----------------
-
 module "vm_module" {
-  source     = "git::https://github.com/dikshagupta12345/vm-instance-module.git?ref=main"  
-  instances = var.instances
+  source = "git::https://github.com/dikshagupta12345/vm-module.git?ref=main"
+
+  for_each = var.instances
+
+  project_id    = each.value.project_id
+  instance_name = each.value.instance_name
+  machine_type  = each.value.machine_type
+  zone          = each.value.zone
+  image         = each.value.image
+  network       = each.value.network
+  subnetwork    = each.value.subnetwork
+  external_ip   = each.value.external_ip
+  sa_name       = var.sa_name
 }
