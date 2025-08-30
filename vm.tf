77d2f4b401263aa2f934b3d5bf27a33b9e65c03a
dikshagupta12345 <diksha.cloud2025@gmail.com>
2025-08-30 19:26:17 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,13 +1,22 @@
 module "vm_module" {
-  source      = "git::https://github.com/dikshagupta12345/vm-instance-module.git?ref=main"
+  source     = "git::https://github.com/dikshagupta12345/vm-instance-module.git?ref=main"
+  project_id = "innate-bucksaw-464410-e8"
+  sa_name    = "test-sa"
 
-  instance_name  = "app"
-  machine_type   = "e2-small"
-  region         = "us-central1"
-  zone           = "us-central1-a"
-  project_id     = "innate-bucksaw-464410-e8"
-  instance_count = 2
-  network = "testvpc"
-  subnetwork = "subnetwork2"
-  sa_name = "test-sa"
+  instances = {
+    "app-vm1" = {
+      machine_type = "e2-small"
+      zone         = "us-central1-a"
+      image        = "debian-cloud/debian-11"
+      network      = "vpc1"
+      subnetwork   = "subnet1"
+    }
+    "db-vm1" = {
+      machine_type = "n1-standard-2"
+      zone         = "us-central1-b"
+      image        = "debian-cloud/debian-11"
+      network      = "vpc1"
+      subnetwork   = "subnet2"
+    }
+   }
 }
