4659f7c2a269dddc809afb8816d908e0a1a8c15e
dikshagupta12345 <diksha.cloud2025@gmail.com>
2025-08-30 21:49:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,13 +10,15 @@ variable "networks" {
 }
 
 variable "instances" {
-  description = "Map of VM instances with configs"
+  description = "Map of VM configurations"
   type = map(object({
-    machine_type = string
-    zone         = string
-    image        = string
-    network      = string
-    subnetwork   = string
-    external_ip  = bool
+    project_id   = string
+    instance_name = string
+    machine_type  = string
+    zone          = string
+    image         = string
+    network       = string
+    subnetwork    = string
+    external_ip   = bool
   }))
 }
