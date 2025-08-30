1ffdbd435f11df4c1f8213c0eb22083bb693f87d
dikshagupta12345 <diksha.cloud2025@gmail.com>
2025-08-30 19:27:43 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,15 @@
 module "vpc_module" {
-  source      = "git::https://github.com/dikshagupta12345/vpc-module.git?ref=main"
+  source     = "git::https://github.com/dikshagupta12345/vpc-module.git?ref=main"
+  project_id = "innate-bucksaw-464410-e8"
 
-  region         = "us-central1"
-  project_id     = "innate-bucksaw-464410-e8"
-  network = "testvpc"
-  subnetwork = "subnetwork2"
+  vpcs = {
+    vpc1 = {}
+    vpc2 = {}
+  }
+
+  subnets = [
+    { name = "subnet1", cidr = "10.1.0.0/24", region = "us-central1", vpc = "vpc1" },
+    { name = "subnet2", cidr = "10.2.0.0/24", region = "us-central1", vpc = "vpc1" },
+    { name = "subnet3", cidr = "10.3.0.0/24", region = "us-east1", vpc = "vpc2" },
+  ]
 }
