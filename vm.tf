df1b3e571608ac550c4462de5cee07dc0bb64d2c
dikshagupta12345 <diksha.cloud2025@gmail.com>
2025-08-30 18:31:15 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,5 +1,5 @@
 module "vm_module" {
-  source      = "git::https://github.com/dikshagupta12345/main-module.git?ref=main"
+  source      = "git::https://github.com/dikshagupta12345/vm-instance-module.git?ref=main"
 
   instance_name  = "app"
   machine_type   = "e2-small"
