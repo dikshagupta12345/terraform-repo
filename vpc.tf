3440c2b1ae2967e8f8b789326b16a4e6179d9bce
dikshagupta12345 <diksha.cloud2025@gmail.com>
2025-08-31 17:37:58 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,10 @@
 module "vpc_networks" {
   for_each = var.networks
-  source     = "git::https://github.com/dikshagupta12345/vpc-module.git?ref=main"  
+  source   = "git::https://github.com/dikshagupta12345/vpc-module.git?ref=main"
 
   project_id = each.value.project_id
   region     = each.value.region
   subnets    = each.value.subnets
+  vpc_name   = each.key   # 👈 ensures uniqueness, even if region is same
 }
+
