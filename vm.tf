b3396b3a5af430273da06974a44925b4d1d4f14a
dikshagupta12345 <diksha.cloud2025@gmail.com>
2025-09-02 13:27:26 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,5 +1,5 @@
 module "vm_module" {
-  source = "git::https://github.com/dikshagupta12345/vm-instance-module.git?ref=main"
+  source = "git::https://github.com/dikshagupta12345/vm-instance-module.git?ref=v1.0.0"
   instances = var.instances
   service_account       = var.service_account
 }
